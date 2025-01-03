c71a281cfd2fc9318bb6da30c35b2d60f22e630a
Soham Thodge <thodge.soham@webseeder.tech>
2025-01-03 17:06:21 +0530

    Revert "Commit for Frontend files"
    This reverts commit 3b2465894e3eb66ba6fd2f3bc4c51dc9693f99ad.

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Include jsx and tsx for React components
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
