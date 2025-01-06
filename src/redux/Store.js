b0ffdc2a29891bb150df57434c68e5e94f7b2531
Dipto-Neogi07 <neogi.dipto@webseeder.tech>
2025-01-06 16:01:40 +0530

    saved in local storage using redux middleware

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,12 +1,23 @@
-// store.js
-import { configureStore } from "@reduxjs/toolkit";
-import userSlice from "./user/Userslice"  ; // Update the path to where your userSlice file is located
-import panditSlice from "./pandit/Panditslice"
+// File: redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import userSlice from "./user/Userslice"  ;
+import panditSlice from './pandit/Panditslice';
+
+// Middleware to save state to localStorage on every state change
+const saveStateMiddleware = (store) => (next) => (action) => {
+  const result = next(action);
+  const state = store.getState();
+  localStorage.setItem('pandits', JSON.stringify(state.pandits));
+  return result;
+};
+
 const store = configureStore({
   reducer: {
-           userdata:userSlice, 
-           pandits:panditSlice
+    userdata:userSlice, 
+    pandits: panditSlice
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(saveStateMiddleware),
 });
 
 export default store;
