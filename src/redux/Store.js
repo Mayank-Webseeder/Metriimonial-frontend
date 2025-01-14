5ed2226db30c500257c673243ca8bf1d347c98b1
Soham Thodge <thodge.soham@webseeder.tech>
2025-01-14 17:59:41 +0530

    Updated reduxing for Kathavachak

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./user/Userslice";
 import panditSlice from "./pandit/Panditslice";
 import commonFormSlice from "../redux/commonform/Commonfromslice";
+import kathavachakReducer from "../redux/kathavachak/KathavachakSlice";
 
 const store = configureStore({
   reducer: {
     userdata: userSlice,
     panditData: panditSlice,
     commonform: commonFormSlice, // Updated key name
+    kathavachaks: kathavachakReducer,
   },
 });
 
